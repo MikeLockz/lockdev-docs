08058f788077a895b6991fe2cbb4a5e7e075cc95
Michael Lockwitz <mlockwitz@gmail.com>
2022-11-16 03:21:06 +0000

    Update config

--- lockdev-docs/docusaurus.config.js
+++ lockdev-docs/docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/mikelockz/lockdev-docs",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -83,7 +83,7 @@ const config = {
       footer: {
         style: "dark",
         links: [],
-        copyright: ``,
+        copyright: `2012`,
       },
       prism: {
         theme: lightCodeTheme,
