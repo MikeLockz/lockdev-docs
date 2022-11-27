fb80909921bb0365228877ea90a810bf03c5bdea
Mike Lockwitz <mlockwitz@gmail.com>
2022-11-26 19:09:19 -0700

    Move entire directory up a level

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/mbp/Development/lockdev-docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/mbp/Development/lockdev-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/mbp/Development/lockdev-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/mbp/Development/lockdev-docs/src/css/custom.css'),
+];
