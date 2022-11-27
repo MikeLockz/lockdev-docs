e90c221a517652ff370673abbf46e30c6eca4620
Mike Lockwitz <mlockwitz@gmail.com>
2022-11-26 19:48:49 -0700

    Update blog edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/mikelockz/lockdev-docs",
+          editUrl: "https://github.com/MikeLockz/lockdev-docs/tree/trunk/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
